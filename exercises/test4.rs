eedc1726b5833b12b9f51dbf21b5016123ea5e24
tonyxwz <tonyxwz@yahoo.com>
2020-05-12 13:33:53 +0200

    test (debugging)

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,8 +5,11 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
-
+macro_rules! my_macro {
+    ($word:expr) => {
+        ["Hello", $word].join(" ")
+    };
+}
 #[cfg(test)]
 mod tests {
     use super::*;
